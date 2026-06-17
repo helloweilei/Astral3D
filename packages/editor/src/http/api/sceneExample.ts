bc96a97a08c8a0363e2a6919d04502479414287b
魏磊 <15718965+charlie_wei@user.noreply.gitee.com>
2026-06-17 21:14:11 +0800

    update

--- packages/editor/src/http/api/sceneExample.ts
+++ packages/editor/src/http/api/sceneExample.ts
@@ -4,28 +4,27 @@
  * @date   2024/7/28 14:54
  * @description 示例场景
  */
-import {request} from "@/http/request";
-import {Service} from "~/network";
+import { request } from "@/http/request";
 
 /**
  * 获取所有示例场景
  */
 export function fetchSceneExampleList(params) {
-    return request.get<Service.ListPageResult<ISceneFetchData>>("/editor3d/sceneExample",{params});
+	return request.get<Service.ListPageResult<ISceneFetchData>>("/editor3d/sceneExample", { params });
 }
 
 /**
  * 获取示例场景
  */
 export function fetchSceneExample(id) {
-    return request.get(`/editor3d/sceneExample/${id}`);
+	return request.get(`/editor3d/sceneExample/${id}`);
 }
 
 /**
  * 新增示例场景
  */
 export function fetchAddSceneExample(data) {
-    return request.post(`/editor3d/sceneExample`,data);
+	return request.post(`/editor3d/sceneExample`, data);
 }
 
 /**
@@ -33,5 +32,5 @@ export function fetchAddSceneExample(data) {
  * @param {number} id
  */
 export function fetchDeleteSceneExample(id: number) {
-    return request.delete(`/editor3d/sceneExample/${id}`,{});
+	return request.delete(`/editor3d/sceneExample/${id}`, {});
 }
